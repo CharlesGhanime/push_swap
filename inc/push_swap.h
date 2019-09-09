9855e19cbacd4449cc58facfc399fb2ff76f8898
Charles <ghanime.charles@gmail.com>
2019-09-09 06:59:48 +0200

    merge and quick to do and heap

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: cghanime <cghanime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 11:30:38 by cghanime          #+#    #+#             */
-/*   Updated: 2019/09/09 05:04:07 by cghanime         ###   ########.fr       */
+/*   Updated: 2019/09/09 05:19:55 by cghanime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,8 +85,7 @@ void					ps_rrr(t_list *stack_a, t_list *stack_b,
 ** PUSH_SWAP FUNCTIONS
 */
 
-// check if liste triee
-// check if liste triee decroissante
-// check si juste un element ou deux a inverser?
+int		check_list_sorted(t_list *stack, t_list **head);
+int		check_list_reverse_sorted(t_list *stack, t_list **head);
 
 #endif
--- srcs/check_list.c
+++ srcs/check_list.c
@@ -6,7 +6,7 @@
 /*   By: cghanime <cghanime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 05:04:34 by cghanime          #+#    #+#             */
-/*   Updated: 2019/09/09 05:12:07 by cghanime         ###   ########.fr       */
+/*   Updated: 2019/09/09 05:18:33 by cghanime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,23 @@ int		check_list_sorted(t_list *stack, t_list **head)
 	while (tmp->next)
 	{
 		if (tmp->value < tmp->next->value)
+			continue;
+		else
+			return (-1);
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+int		check_list_reverse_sorted(t_list *stack, t_list **head)
+{
+	t_list *tmp;
+
+	*head = stack;
+	tmp = stack;
+	while (tmp->next)
+	{
+		if (tmp->value > tmp->next->value)
 			continue;
 		else
 			return (-1);
