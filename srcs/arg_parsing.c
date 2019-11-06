ab193212a9c39e61f9966c2f48e07577c648b3fa
Charles <ghanime.charles@gmail.com>
2019-11-06 12:53:45 +0100

    arg parsing a tester + strjoinfree etc

--- srcs/arg_parsing.c
+++ srcs/arg_parsing.c
@@ -11,3 +11,31 @@
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
+
+char	**arg_parsing(char **argv, int argc)
+{
+	int i;
+	char *input;
+	char **output;
+
+	i = 1;
+	input = NULL;
+	output = NULL;
+	while (i < argc)
+	{ 
+		input = ft_strjoin(input, argv[i]);
+		i++;
+	}
+	output = ft_strsplit(input, ' ');
+	printf("output = %s\n", output);
+	return (&output);
+}
+
+int		main(int argc, char **argv)
+{
+	char **output;
+
+	output = NULL;
+	output = arg_parsing(argv, argc);
+	return (0);
+}
