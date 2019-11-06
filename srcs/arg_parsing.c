e449eaf24580e73c3ab6cdd9a6c3394701afd911
Charles <ghanime.charles@gmail.com>
2019-11-06 18:08:54 +0100

    faire des fonctions pour les tab dans la libft

--- srcs/arg_parsing.c
+++ srcs/arg_parsing.c
@@ -6,29 +6,31 @@
 /*   By: cghanime <cghanime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/01 20:56:32 by cghanime          #+#    #+#             */
-/*   Updated: 2019/11/01 21:59:14 by cghanime         ###   ########.fr       */
+/*   Updated: 2019/11/06 17:59:23 by cghanime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
-char	**arg_parsing(char **argv, int argc)
+char	**arg_parsing(char **av)
 {
 	int i;
-	char *input;
+	char **input;
 	char **output;
 
-	i = 1;
+	i = 0;
 	input = NULL;
 	output = NULL;
-	while (i < argc)
+	while (av[i])
 	{ 
-		input = ft_strjoin(input, argv[i]);
+		input = ft_strsplit(av[i], ' ');
 		i++;
 	}
-	output = ft_strsplit(input, ' ');
-	printf("output = %s\n", output);
-	return (&output);
+	printf("input = %s\n", input);
+//	output = ft_strsplit(input, ' ');
+//	printf("output = %s\n", output);
+//	return (output);
+	return (NULL);
 }
 
 int		main(int argc, char **argv)
